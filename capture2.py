f9e0b5037586088c4fdf6199b132c26b6b4cf819
Nik212 <gevnik08@gmail.com>
2019-10-05 11:43:54 +0300

    detection

--- capture2.py
+++ capture2.py
@@ -15,15 +15,15 @@ while (cap.isOpened()):
 		mask = cv2.erode(mask, None, iterations = 2)
 		mask = cv2.dilate(mask, None, iterations = 4)
 
-		cont = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-		cont = cont[0]
-		if (cont):
+		_, cont, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+		f = any([item.any() for item in cont])
+		if (f):
 			best_fit = max(cont, key=cv2.contourArea)
 			cv2.drawContours(frame, best_fit, 0, (255, 0, 255), 3)
 			(x, y, w, h) = cv2.boundingRect(best_fit)
 			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 3)
 			coordinates.append((int((x+(x+w)/2)), int((y+(y+h)/2))))
-			cv2.drawMarker(frame, (int((x+(x+w))/2), int((y+(y+h))/2)), (0, 255, 255))
+			cv2.drawMarker(frame,(int((x+(x+w))/2), int((y+(y+h))/2)), (0, 255, 255))
 			cv2.imwrite("image_processed.png", frame)
 			break
 	else:
